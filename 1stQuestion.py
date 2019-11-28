8a34311365f02dfd49a3c16ee479e66a6bfd11f3
AnujSaxena19 <19anujsaxena19@gmail.com>
2019-11-28 22:16:09 +0530

    Initial commit

---
+++ 1stQuestion.py
@@ -0,0 +1,12 @@
+class Myexception(Exception):
+    pass
+def xyz(a,b):
+    c=a+b
+    if(c<150):
+        raise Myexception("Custom Exception Occur")
+    else:
+        print("Sum is" +str(c))
+
+a=int(input())
+b=int(input())
+xyz(a,b)
